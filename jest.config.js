c6a46135bdd2ddda3c53b127240a7131d414d98b
hasangawr <hasangawr@gmail.com>
2025-10-29 00:12:41 +0530

    Disable coverage collection settings in Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   testEnvironment: "node",
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov", "html"],
+  // collectCoverage: true,
+  // coverageDirectory: "coverage",
+  // coverageReporters: ["text", "lcov", "html"],
   testMatch: ["**/*.test.js"],
   verbose: true,
   forceExit: true,
   clearMocks: true,
-  collectCoverageFrom: ["app.js", "!node_modules/**", "!coverage/**"],
+  // collectCoverageFrom: ["app.js", "!node_modules/**", "!coverage/**"],
 };
